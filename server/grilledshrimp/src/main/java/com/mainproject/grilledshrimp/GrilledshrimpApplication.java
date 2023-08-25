f0d90657fc8e16aa3e5dc00f6576564163b86618
prinzusseugen <74959653+prinzusseugen@users.noreply.github.com>
2023-08-25 16:40:25 +0900

    :tada: init commit

---
+++ server/grilledshrimp/src/main/java/com/mainproject/grilledshrimp/GrilledshrimpApplication.java
@@ -0,0 +1,13 @@
+package com.mainproject.grilledshrimp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrilledshrimpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrilledshrimpApplication.class, args);
+	}
+
+}
