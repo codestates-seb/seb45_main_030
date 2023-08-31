9b17ff49c3724faa0a4b5f398fc20f0901cd3057
toa <gimhyoungsoo@gmail.com>
2023-08-31 18:09:27 +0900

    💩 메인 페이지 작성 중

---
+++ client/src/pages/MainPage.jsx
@@ -0,0 +1,16 @@
+function MainPage() {
+    const Banner = () => {
+
+    };
+    return (
+        <>
+            <header>헤더 컴포넌트</header>
+            <Banner></Banner>
+            <div>ImageList
+                <div>ImageItem</div>
+            </div>
+            
+        </>
+    );
+}
+export default MainPage;
