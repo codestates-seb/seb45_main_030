410014dfbe2cc8bf3d028713f7df98efaf5277c0
toa <gimhyoungsoo@gmail.com>
2023-08-31 18:52:19 +0900

    💡 구성을 위한 주석 추가

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -6,8 +6,12 @@ function MainPage() {
         <>
             <header>헤더 컴포넌트</header>
             <Banner></Banner>
-            <div>ImageList
-                <div>ImageItem</div>
+
+            {/* 위치정보가 있으면 인근 뷰포인트 */}
+            {/* 없으면 인기 뷰포인트 */}
+            
+            <div>ImageList 컴포넌트
+                <div>ImageItem 컴포넌트</div>
             </div>
             
         </>
