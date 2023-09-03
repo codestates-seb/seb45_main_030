3359f36183eb91e2b1b322e4e72b7960a0639335
toa <gimhyoungsoo@gmail.com>
2023-09-03 20:01:45 +0900

    🚧 데이터 fetch 가능, 무한 스크롤을 위한 작업 중

--- client/src/components/Image/ImageList.jsx
+++ client/src/components/Image/ImageList.jsx
@@ -1,23 +1,33 @@
 import ImageItem from "./ImageItem";
 import styles from "./ImageList.module.css";
-function ImageList({ data }) {
-    //     /* ItemList에 세 줄의 container가 있고 container 안에 ImageItem이 3개의 container에 돌아가며 props로 전달되어
-    //                 1 2 3
-    //                 4 5 6
-    //                 7 8 9
-    //                 같은 형식으로 출력됨
+import useGetData from "../../hooks/useGetData";
+import useIntObserve from "../../hooks/useIntObseve";
+function ImageList() {
 
-    //                 1 4 7
-    //                 2 5 8
-    //                 3 6 9
-    //                 이 방식은 다음 이미지 묶음을 불러오기 전에 비어있는 이미지가 세로로 위치 하는 문제가 있음
-    //             */
+
+    // ```
+    // useGetAPI로 데이터를 가져온다
+    // 스크롤의 끝을 감지해 isScrollEnd가 true일 경우 
+    // url의 쿼리스트링에서 다음 페이지를 요청한다
+    // 새로 요청한 다음 페이지의 데이터를 이전 이미지 배열에 추가한다 (prev)=>{[...prev, *새 데이터*]}
+    //
+    // https://picsum.photos/ 더미 이미지 API
+    const { imgListData } = useGetData("https://picsum.photos/v2/list?page=2");
+    //
+    // const topGrid = document.querySelector(".top_grid")
+    // const {isScrollEnd} =  useIntObserve(topGrid)
+    //
+    // ```
+
+
+    // 하나의 배열에 데이터를 저장하고 return에서 각 column grid에 뿌리면 
+    // 코드도 간결해지고, 다음 페이지의 데이터를 추가할때도 편할 듯
     const firstColumnArr = [];
     const secondColumnArr = [];
     const thirdColumnArr = [];
 
-    if (data.length !== 0) {
-        data.map((el, idx) => {
+    if (imgListData && imgListData.length !== 0) {
+        imgListData.map((el, idx) => {
             if (idx % 3 === 0) {
                 firstColumnArr.push(el.download_url);
             } else if (idx % 3 === 1) {
@@ -25,13 +35,13 @@ function ImageList({ data }) {
             } else {
                 thirdColumnArr.push(el.download_url);
             }
+            return null;
         });
     }
 
     return (
         <div className={styles.container}>
-            <div className={styles.grid}>
-
+            <div className={styles.top_grid}>
                 <div className={styles.column_grid}>
                     {firstColumnArr.map((el) => (
                         <ImageItem src={el} />
@@ -49,7 +59,6 @@ function ImageList({ data }) {
                         <ImageItem src={el} />
                     ))}
                 </div>
-                
             </div>
         </div>
     );
