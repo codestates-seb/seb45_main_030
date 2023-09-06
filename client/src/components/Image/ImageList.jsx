e00cb857d73a0241f7bb107d6f8c4c26529e8b16
toa <gimhyoungsoo@gmail.com>
2023-09-06 12:39:42 +0900

    ✨ 무한 스크롤 기능 구현 - 이미지 뿌리는 코드 추가 - 커스텀 훅 추가 - 둘다 리팩터링 가능

--- client/src/components/Image/ImageList.jsx
+++ client/src/components/Image/ImageList.jsx
@@ -1,65 +1,50 @@
 import ImageItem from "./ImageItem";
 import styles from "./ImageList.module.css";
-import useGetData from "../../hooks/useGetData";
-import useIntObserve from "../../hooks/useIntObseve";
+import useInfiniteGetImage from "../../hooks/useInfiniteGetImage";
+
 function ImageList() {
-
-
-    // ```
-    // useGetAPI로 데이터를 가져온다
-    // 스크롤의 끝을 감지해 isScrollEnd가 true일 경우 
-    // url의 쿼리스트링에서 다음 페이지를 요청한다
-    // 새로 요청한 다음 페이지의 데이터를 이전 이미지 배열에 추가한다 (prev)=>{[...prev, *새 데이터*]}
-    //
-    // https://picsum.photos/ 더미 이미지 API
-    const { imgListData } = useGetData("https://picsum.photos/v2/list?page=2");
-    //
-    // const topGrid = document.querySelector(".top_grid")
-    // const {isScrollEnd} =  useIntObserve(topGrid)
-    //
-    // ```
-
-
-    // 하나의 배열에 데이터를 저장하고 return에서 각 column grid에 뿌리면 
-    // 코드도 간결해지고, 다음 페이지의 데이터를 추가할때도 편할 듯
-    const firstColumnArr = [];
-    const secondColumnArr = [];
-    const thirdColumnArr = [];
-
-    if (imgListData && imgListData.length !== 0) {
-        imgListData.map((el, idx) => {
-            if (idx % 3 === 0) {
-                firstColumnArr.push(el.download_url);
-            } else if (idx % 3 === 1) {
-                secondColumnArr.push(el.download_url);
-            } else {
-                thirdColumnArr.push(el.download_url);
-            }
-            return null;
-        });
-    }
+    const columns = {
+        first: [],
+        second: [],
+        third: [],
+    };
+    const ImageDataDistributor = () => {
+        if (state.dataArr.length !== 0) {
+            state.dataArr.map((el, idx) => {
+                if (idx % 3 === 0) {
+                    columns.first.push(el);
+                } else if (idx % 3 === 1) {
+                    columns.second.push(el);
+                } else {
+                    columns.third.push(el);
+                }
+                return null;
+            });
+        }
+    };
+    const { state, sentinelRef } = useInfiniteGetImage("https://picsum.photos/v2/list");
+    ImageDataDistributor();
 
     return (
         <div className={styles.container}>
             <div className={styles.top_grid}>
                 <div className={styles.column_grid}>
-                    {firstColumnArr.map((el) => (
-                        <ImageItem src={el} />
-                    ))}
+                    {columns.first.map((el) => {
+                        return <ImageItem key={el.id} src={el.download_url} />;
+                    })}
                 </div>
-
                 <div className={styles.column_grid}>
-                    {secondColumnArr.map((el) => (
-                        <ImageItem src={el} />
-                    ))}
+                    {columns.second.map((el) => {
+                        return <ImageItem key={el.id} src={el.download_url} />;
+                    })}
                 </div>
-
                 <div className={styles.column_grid}>
-                    {thirdColumnArr.map((el) => (
-                        <ImageItem src={el} />
-                    ))}
+                    {columns.third.map((el) => {
+                        return <ImageItem key={el.id} src={el.download_url} />;
+                    })}
                 </div>
             </div>
+            <div ref={sentinelRef}></div>
         </div>
     );
 }
--- client/src/hooks/useIntObseve.jsx
+++
@@ -1,7 +0,0 @@
-function useIntObserve(element) {
-    let isScrollEnd;
-    const io = new IntersectionObserver((entries, observer) => {})
-    io.observe(element)
-    return {isScrollEnd}
-}
-export default useIntObserve;
