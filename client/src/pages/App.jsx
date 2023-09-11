158f5b4ac90f3b344f114c6aade0578b81da9837
schani3 <schani3931@gmail.com>
2023-09-11 13:57:17 +0900

    [FE] 23) MyPageBtn 삭제 및 MyPageBtn과 연동했던 라우팅 코드 삭제

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MyPage from "./MyPage";
-import MyPhotos from "../components/my_page/MyPhotos";
-import BookmarkFolder from "../components/my_page/BookmarkFolder";
-import Comments from "../components/my_page/Comments";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route exact path="/mypage/" element={<MyPage />} />
-                <Route path="/mypage/my-photos" element={<MyPhotos />} />
-                <Route path="/mypage/bookmark" element={<BookmarkFolder />} />
-                <Route path="/mypage/comments" element={<Comments />} />
             </Routes>
         </BrowserRouter>
     );
