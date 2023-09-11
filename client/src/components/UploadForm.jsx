b8c1bdc1016d6b4653184679451901d8728b429b
Yeji Cho <chos41379@gmail.com>
2023-09-11 15:04:22 +0900

    게시글입력화면

--- client/src/components/UploadForm.jsx
+++ client/src/components/UploadForm.jsx
@@ -1,43 +1,83 @@
 import React, { useRef, useState } from "react";
 import styles from "./UploadForm.module.css";
 import axios from "axios";
+import { BsTrash3Fill } from "react-icons/bs";
+import { AiFillCloseCircle } from "react-icons/ai";
 
 export default function UploadForm({ onClose }) {
     const [image, setImage] = useState(null);
     const [place, setPlace] = useState("");
     const [description, setDescription] = useState("");
-    // 해시태그를 저장할 배열
     const initialTags = [];
     const [tags, setTags] = useState(initialTags);
-
-    // 업로딩 중인지, 업로드가 성공했는지 나타내는 상태 변수 생성
+    const [uploading, setUploading] = useState(false);
+    const [uploadSuccess, setUploadSuccess] = useState(false);
+    const [uploadError, setUploadError] = useState("");
 
     const removeTags = (indexToRemove) => {
         const filter = tags.filter((el, index) => index !== indexToRemove);
         setTags(filter);
     };
+
     const addTags = (event) => {
-        // tags 배열에 새로운 태그를 추가하는 메소드
-        const inputVal = event.target.value;
-        // 이미 입력되어 있는 태그인지 검사하여 이미 있는 태그라면 추가하지 말기
-        // 아무것도 입력하지 않은 채 Enter 키 입력시 메소드 실행하지 말기
-        // 태그가 추가되면 input 창 비우기
-        if (event.key.toLowerCase().includes("enter") && inputVal !== "" && !tags.includes(inputVal)) {
-            // setTags([...tags, inputVal]);
-            setTags((prev) => [...prev, inputVal]);
-            event.target.value = "";
+        if (event.key.toLowerCase() === "enter") {
+            // 엔터 키가 입력되었을 때만 실행
             event.preventDefault();
+            const inputVal = event.target.value.trim();
+            if (inputVal !== "" && !tags.includes(inputVal)) {
+                setTags((prev) => [...prev, inputVal]);
+                event.target.value = "";
+            }
         }
     };
-    // 이미지 변경 핸들러
-    const handleImageChange = (e) => {
-        const file = e.target.files[0];
+
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
         if (file) {
             const reader = new FileReader();
-            reader.onload = (e) => {
-                setImage(e.target.result);
+            reader.onload = (event) => {
+                setImage(event.target.result);
             };
             reader.readAsDataURL(file);
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append("post_image", image);
+        formData.append("post_title", place);
+        formData.append("post_caption", description);
+        formData.append("post_hashtag", tags);
+
+        setUploading(true);
+
+        try {
+            const response = await axios.post("http://localhost:8080/posts", formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            });
+            console.log(response);
+
+            setUploadSuccess(true);
+            console.log(uploadSuccess);
+            setImage(null);
+            setPlace("");
+            setDescription("");
+            setTags([]);
+            onClose();
+
+            // 업로드 성공 시 alert 창 표시
+            alert("업로드에 성공했습니다!");
+        } catch (error) {
+            setUploadError("죄송합니다. 문제가 발생했습니다.");
+            console.log(uploadError);
+            alert(
+                "죄송합니다. 문제가 발생했습니다. 잠시 후 다시 시도해주시거나, 페이지를 새로 고침하여 다시 입력해주세요.",
+            );
+        } finally {
+            setUploading(false);
         }
     };
 
@@ -46,47 +86,27 @@ export default function UploadForm({ onClose }) {
         fileInputRef.current.click();
     };
 
-    // 폼 제출 핸들러
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        // FormData를 사용하여 이미지와 텍스트 데이터를 서버로 전송, 각각의 필드에 파일 및 텍스트 추가
-        const formData = new FormData();
-        formData.append("post_image", image);
-        formData.append("post_title", place);
-        formData.append("post_caption", description);
-        formData.append("post_hashtag", tags);
-        // formData.append("selected", JSON.stringify());
-        // 해시태그 배열을 문자열로 변환하여 전송
-        try {
-            const response = await axios.post("http://localhost:8080/posts", formData, {
-                headers: {
-                    "Content-Type": "multipart/form-data",
-                },
-            });
-            // response 변수를 사용하여 서버 응답 데이터를 처리
-            console.log("서버 응답", response.data);
-
-            setImage(null);
-            setPlace("");
-            setDescription("");
-            setTags([]);
-            onClose();
-        } catch (error) {
-            console.error("오류 발생", error);
-        }
-    };
-
     return (
         <div className={styles.modal}>
             <div className={styles.modal_content}>
                 <button onClick={onClose} className={styles.close_button}>
-                    닫기
+                    <AiFillCloseCircle />
                 </button>
                 <form onSubmit={handleSubmit} className={styles.form_container}>
                     <label className={styles.image_section}>
                         <div className={styles.image_upload_label} onClick={handleClickUpload}>
                             {image ? (
-                                <img src={image} alt="업로드된 사진" className={styles.image} />
+                                <>
+                                    <img src={image} alt="업로드된 사진" className={styles.image} />
+                                    <button
+                                        type="button"
+                                        className={styles.delete_button}
+                                        // setImage를 null로 처리하여 이미지를 없앰?
+                                        onClick={() => setImage(null)}
+                                    >
+                                        <BsTrash3Fill />
+                                    </button>
+                                </>
                             ) : (
                                 <div className={styles.image_placeholder}>
                                     최고의 사진을 올려주세요
@@ -96,6 +116,7 @@ export default function UploadForm({ onClose }) {
                         </div>
                     </label>
                     <input
+                        name="image"
                         type="file"
                         onChange={handleImageChange}
                         accept="image/*"
@@ -137,22 +158,23 @@ export default function UploadForm({ onClose }) {
                                 ))}
                             </ul>
                             <input
+                                name="tagInput"
                                 className={styles.tag_input}
                                 type="text"
-                                //키보드의 Enter 키에 의해 addTags 메소드가 실행
-                                onKeyUp={(e) => {
-                                    {
-                                        addTags(e);
-                                    }
-                                }}
+                                onKeyUp={(e) => addTags(e)}
                                 placeholder="최대 3개까지 #태그를 입력해주세요 (예시) #바다"
                             />
                         </div>
                     </div>
-                    <button type="submit" className={styles.submit_button}>
-                        게시하기
-                    </button>
                 </form>
+                <button
+                    type="submit"
+                    className={styles.submit_button}
+                    disabled={uploading} // 업로드 중일 때 버튼 비활성화
+                    onClick={handleSubmit}
+                >
+                    {uploading ? "업로드 중..." : "게시하기"}
+                </button>
             </div>
         </div>
     );
