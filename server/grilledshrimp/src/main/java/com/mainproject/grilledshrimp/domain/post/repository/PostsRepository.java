fc25b574fabf83d25c3722d706c6047c98cd355d
prinzusseugen <74959653+prinzusseugen@users.noreply.github.com>
2023-09-13 17:37:55 +0900

    :recycle: 페이징을 위한 쿼리문 추가

--- server/grilledshrimp/src/main/java/com/mainproject/grilledshrimp/domain/post/repository/PostsRepository.java
+++ server/grilledshrimp/src/main/java/com/mainproject/grilledshrimp/domain/post/repository/PostsRepository.java
@@ -1,8 +1,13 @@
 package com.mainproject.grilledshrimp.domain.post.repository;
 
 import com.mainproject.grilledshrimp.domain.post.entity.Posts;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
-
+    Page<Posts> findAll(Pageable pageable);
+    @Query("SELECT p FROM Posts p ORDER BY p.postId DESC")
+    Page<Posts> findPosts(Pageable pageable);
 }
