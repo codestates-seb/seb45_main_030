3e7d1ed5ef96f051bca34bd54ee98877a16791f8
prinzusseugen <74959653+prinzusseugen@users.noreply.github.com>
2023-09-13 10:11:23 +0900

    :recycle: 컬럼 추가 및 상세 설정 추가

--- server/grilledshrimp/src/main/java/com/mainproject/grilledshrimp/domain/post/entity/Posts.java
+++ server/grilledshrimp/src/main/java/com/mainproject/grilledshrimp/domain/post/entity/Posts.java
@@ -1,60 +1,86 @@
 package com.mainproject.grilledshrimp.domain.post.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.mainproject.grilledshrimp.domain.bookmark.entity.Bookmark;
+import com.mainproject.grilledshrimp.domain.post.dto.PostsPostDto;
 import com.mainproject.grilledshrimp.domain.postTag.entity.PostTag;
 import com.mainproject.grilledshrimp.domain.recommendPost.entity.RecommendPost;
+import com.mainproject.grilledshrimp.domain.tag.entity.Tag;
 import com.mainproject.grilledshrimp.domain.user.entity.Users;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity
+@Builder
 public class Posts {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
     private Long postId;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
-    private Users user;
+    private Users users;
 
-    @Column(nullable = false)
+    @Column(name = "post_title", nullable = false)
     private String postTitle;
 
-    @Column(nullable = true)
+    @Column(name = "post_caption", nullable = true)
     private String postCaption;
 
-    @Column(nullable = false)
+    //@Column(name = "post_image", nullable = false)
     private String postImage;
 
-    @Column(nullable = true)
+    @Column(name = "post_address", nullable = true)
     private String postAddress;
 
-    @Column(nullable = false)
+    @Column(name = "post_comment_permission", nullable = false)
     private boolean postCommentPermission;
 
-    @Column(nullable = false)
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt = LocalDateTime.now();
 
-    @Column(nullable = false)
+    @UpdateTimestamp
     private LocalDateTime modifiedAt;
 
-    @OneToMany(mappedBy = "posts", orphanRemoval = true)
+    @OneToMany(mappedBy = "posts", cascade = CascadeType.PERSIST, orphanRemoval = true)
     private List<Bookmark> bookmarks = new ArrayList<>();
 
-    @OneToMany(mappedBy = "posts", orphanRemoval = true)
-    private List<PostTag> postTags = new ArrayList<>();
+    @OneToMany(mappedBy = "posts", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    @JsonManagedReference
+    private List<PostTag> postTags;
 
-    @OneToMany(mappedBy = "posts", orphanRemoval = true)
+    @OneToMany(mappedBy = "posts", cascade = CascadeType.PERSIST, orphanRemoval = true)
     private List<RecommendPost> recommendPostList = new ArrayList<>();
+
+    public void addUser(Users user){
+        this.users = user;
+        if(!user.getPosts().contains(this)){
+            user.getPosts().add(this);
+        }
+    }
+
+    public List<PostTag> getPostTags() {
+        if (postTags == null) {
+            postTags = new ArrayList<>();
+        }
+        return postTags;
+    }
+
+
 }
