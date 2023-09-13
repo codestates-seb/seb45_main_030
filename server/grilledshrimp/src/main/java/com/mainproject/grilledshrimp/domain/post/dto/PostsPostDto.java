5eef649998d8e788eba102ea120a596ede88c783
prinzusseugen <74959653+prinzusseugen@users.noreply.github.com>
2023-09-13 10:11:49 +0900

    :recycle: 빌더 패턴 추가 및 순환 참조 방지

--- server/grilledshrimp/src/main/java/com/mainproject/grilledshrimp/domain/post/dto/PostsPostDto.java
+++ server/grilledshrimp/src/main/java/com/mainproject/grilledshrimp/domain/post/dto/PostsPostDto.java
@@ -1,23 +1,46 @@
 package com.mainproject.grilledshrimp.domain.post.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mainproject.grilledshrimp.domain.post.entity.Posts;
+//import com.mainproject.grilledshrimp.domain.postTag.entity.PostTag;
+import com.mainproject.grilledshrimp.domain.tag.entity.Tag;
+import com.mainproject.grilledshrimp.domain.user.entity.Users;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotNull;
 import java.util.List;
+import java.util.Set;
 
 // 게시글 생성
 @Data
 public class PostsPostDto {
-    private Long user_id;
+    @JsonProperty("userId")
+    private Long userId;
 
     @NotNull
     @Length(min = 1, max = 100)
-    private String post_title;
-    private String post_address;
-    private String post_caption;
-    private boolean post_comment_permission;
+    private String postTitle;
+    private String postCaption;
+    private String postImage;
+    private String postAddress;
+    private boolean postCommentPermission;
+    @JsonProperty("tags")
     private List<String> tags;
-    private List<String> post_image;
+
+    public Posts toPosts(Users user){
+        Posts post = Posts.builder()
+                .users(user)
+                .postTitle(postTitle)
+                .postCaption(postCaption)
+                .postImage(postImage)
+                .postAddress(postAddress)
+                .postCommentPermission(postCommentPermission)
+                .build();
+        post.addUser(user);
+        return post;
+    }
 
 }
