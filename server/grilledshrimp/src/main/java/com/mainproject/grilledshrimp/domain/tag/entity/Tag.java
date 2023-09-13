1d1fc0116a2de9c2411e135e7caf518aa1310a37
prinzusseugen <74959653+prinzusseugen@users.noreply.github.com>
2023-09-13 10:13:51 +0900

    :recycle: 컬럼 추가

--- server/grilledshrimp/src/main/java/com/mainproject/grilledshrimp/domain/tag/entity/Tag.java
+++ server/grilledshrimp/src/main/java/com/mainproject/grilledshrimp/domain/tag/entity/Tag.java
@@ -1,25 +1,23 @@
 package com.mainproject.grilledshrimp.domain.tag.entity;
 
-import com.mainproject.grilledshrimp.domain.postTag.entity.PostTag;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.mainproject.grilledshrimp.domain.post.entity.Posts;
+import lombok.*;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Builder
 @Setter
 @Entity
 public class Tag {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tag_id")
     private Long id;
 
     @Column(nullable = false)
-    private String tagName;
+    private String name;
 }
