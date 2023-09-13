bd5ac01fc985c7bf5e2c7d932d813a8222a915f9
tyrue@github.com <kdh9426@naver.com>
2023-09-13 10:32:42 +0900

    :sparkles: [BE] 간단한 유저 정보 반환 DTO 생성

---
+++ server/grilledshrimp/src/main/java/com/mainproject/grilledshrimp/domain/user/dto/UserResponseSimpleDto.java
@@ -0,0 +1,9 @@
+package com.mainproject.grilledshrimp.domain.user.dto;
+
+// 간단한 유저 정보 조회 응답 DTO
+public class UserResponseSimpleDto {
+    private Long userId;
+    private String username;
+    private String email;
+    private String profileImage;
+}
