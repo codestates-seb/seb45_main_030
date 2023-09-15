547c4130af0f9c6153b8e2c220589c5b891523ef
toa <gimhyoungsoo@gmail.com>
2023-09-15 19:25:40 +0900

    🚧 💡백엔드 연동 코드 주석으로 추가

--- client/src/hooks/useInfiniteGetImage.jsx
+++ client/src/hooks/useInfiniteGetImage.jsx
@@ -1,16 +1,11 @@
 import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 
-/**
- * @param {string} url - 쿼리스트링을 제외한 url
- * @returns {object} state { pageNum:{number}, dataArr:{Array}, }
- * @returns {useRef} sentinelRef  ref={ sentinelRef } 속성을 지닌 HTML요소가 화면에 등장하면 다음 사진을 불러옵니다.
- *
- */
 function useGetImageList(url) {
     const sentinelRef = useRef();
+
     const [state, setState] = useState({
-        pageNum: 2,
+        pageNum: 1,
         dataArr: [],
     });
 
@@ -20,7 +15,7 @@ function useGetImageList(url) {
 
         const getApiData = async () => {
             try {
-                const response = await axios.get(`${url}?page=${state.pageNum}`);
+                const response = await axios.get(`https://picsum.photos/v2/list?page=${state.pageNum}`);
                 setState((prevState) => {
                     return {
                         ...prevState,
@@ -33,22 +28,45 @@ function useGetImageList(url) {
                 // 오류 토스트 컴포넌트를 여기에 추가할 수 있습니다.
             }
         };
+
+        // 백엔드 API 적용 코드
+        // const getApiData = async () => {
+        //     try {
+        //         const response = await axios.get(`${url}?page=${state.pageNum}`);
+        //         setState((prevState) => {
+        //             return {
+        //                 ...prevState,
+        //                 pageNum: prevState.pageNum + 1,
+        //                 dataArr: [...prevState.dataArr, ...response.data.data],
+        //             };
+        //         });
+        //     } catch (error) {
+        //         console.error("데이터를 불러오는 중 오류 발생:", error);
+        //         // 오류 토스트 컴포넌트를 여기에 추가할 수 있습니다.
+        //     }
+        // };
+
         const io = new IntersectionObserver((entries, observer) => {
             entries.forEach((entry) => {
                 if (entry.isIntersecting) {
                     getApiData();
+                    observer.unobserve(entry.target)
+                } else {
                 }
             });
         }, {});
+
         if (currentRefValue) {
             io.observe(currentRefValue);
         }
+
         return () => {
             if (currentRefValue) {
                 io.unobserve(currentRefValue);
             }
         };
     }, [url, state.pageNum]);
+
     return { state, sentinelRef };
 }
 export default useGetImageList;
