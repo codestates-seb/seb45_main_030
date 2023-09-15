69827f4b63297bc832ca7562f8729202c076f8f6
prinzusseugen <74959653+prinzusseugen@users.noreply.github.com>
2023-09-15 17:35:50 +0900

    :art: Builder, Data, Getter, AllArgsConstructor 추가

--- server/grilledshrimp/src/main/java/com/mainproject/grilledshrimp/domain/recommend/dto/RecommendPostDto.java
+++ server/grilledshrimp/src/main/java/com/mainproject/grilledshrimp/domain/recommend/dto/RecommendPostDto.java
@@ -1,7 +1,20 @@
 package com.mainproject.grilledshrimp.domain.recommend.dto;
 
+import com.mainproject.grilledshrimp.domain.post.entity.Posts;
+import com.mainproject.grilledshrimp.domain.recommend.entity.Recommend;
+import com.mainproject.grilledshrimp.domain.user.entity.Users;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+
 // 게시글 추천 수정
+@Builder
+@Data
+@Getter
+@AllArgsConstructor
 public class RecommendPostDto {
     private Long post_id;
     private Long user_id;
+
 }
