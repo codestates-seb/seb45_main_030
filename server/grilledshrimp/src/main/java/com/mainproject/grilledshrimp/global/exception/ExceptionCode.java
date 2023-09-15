14e60ffbc0be0f841c735e6dd174e40f12175353
prinzusseugen <74959653+prinzusseugen@users.noreply.github.com>
2023-09-15 17:27:41 +0900

    :art: 추천 관련 추가

--- server/grilledshrimp/src/main/java/com/mainproject/grilledshrimp/global/exception/ExceptionCode.java
+++ server/grilledshrimp/src/main/java/com/mainproject/grilledshrimp/global/exception/ExceptionCode.java
@@ -47,7 +47,10 @@ public enum ExceptionCode {
     // 이미지
     IMAGE_NOT_FOUND(404, "해당 이미지를 찾을 수 없습니다."),
     IMAGE_UPLOAD_FAILED(500, "이미지 업로드에 실패했습니다."),
-    PERMISSION_DENIED(403, "권한이 없습니다.");
+    PERMISSION_DENIED(403, "권한이 없습니다."),
+
+    //추천
+    RECOMMEND_NOT_FOUND(404, "해당 추천을 찾을 수 없습니다.");
 
     private int status;
     private String message;
