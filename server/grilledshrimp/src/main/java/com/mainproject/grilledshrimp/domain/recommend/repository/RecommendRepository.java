6c39e83dc6e3552cf9f2e14bac94a15f22a6687c
prinzusseugen <74959653+prinzusseugen@users.noreply.github.com>
2023-09-15 17:36:14 +0900

    :art: 유저의 유저Id로 추천한 항목을 찾기 위한 기능 추가

--- server/grilledshrimp/src/main/java/com/mainproject/grilledshrimp/domain/recommend/repository/RecommendRepository.java
+++ server/grilledshrimp/src/main/java/com/mainproject/grilledshrimp/domain/recommend/repository/RecommendRepository.java
@@ -5,8 +5,10 @@ import com.mainproject.grilledshrimp.domain.recommend.entity.Recommend;
 import com.mainproject.grilledshrimp.domain.user.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RecommendRepository extends JpaRepository<Recommend, Long>{
     Optional<Recommend> findByUsersAndPosts(Users users, Posts posts);
+    List<Recommend> findByUsers_UserId(Long userId);
 }
