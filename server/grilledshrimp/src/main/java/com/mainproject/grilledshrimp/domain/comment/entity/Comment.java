c8b3784bc9544341a8ee70a5d6747173390e49b6
prinzusseugen <74959653+prinzusseugen@users.noreply.github.com>
2023-09-15 11:07:35 +0900

    :fire: 댓글 추천 삭제

--- server/grilledshrimp/src/main/java/com/mainproject/grilledshrimp/domain/comment/entity/Comment.java
+++ server/grilledshrimp/src/main/java/com/mainproject/grilledshrimp/domain/comment/entity/Comment.java
@@ -1,18 +1,12 @@
 package com.mainproject.grilledshrimp.domain.comment.entity;
-
 import com.mainproject.grilledshrimp.domain.post.entity.Posts;
-import com.mainproject.grilledshrimp.domain.recommendComment.entity.RecommendComment;
 import com.mainproject.grilledshrimp.domain.user.entity.Users;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -22,24 +16,28 @@ public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long comment_id;
-
     @ManyToOne
     @JoinColumn(name = "user_id")
     private Users users;
-
     @ManyToOne
     @JoinColumn(name = "post_id")
     private Posts posts;
-
     @Column(nullable = false)
     private String comment_text;
-
     @Column(nullable = false)
     private LocalDateTime created_at = LocalDateTime.now();
-
     @Column(nullable = false)
     private LocalDateTime modified_at;
 
-    @OneToMany(mappedBy = "comment")
-    private List<RecommendComment> recommendCommentList = new ArrayList<>();
+
+    public void setPost(Posts post) {
+        this.posts = post;
+    }
+    public void setUser(Users user) {
+        this.users = user;
+    }
+
+    public void setCommentText(String commentText) {
+        this.comment_text =commentText;
+    }
 }
