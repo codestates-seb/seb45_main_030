01dbffb12065ac40649e7e5b073858dcaa4c5f49
toa <gimhyoungsoo@gmail.com>
2023-09-17 18:37:46 +0900

    🐛 무한 스크롤 이미지 데이터 페이지 로딩 수정

--- client/src/hooks/useInfiniteGetImage.jsx
+++ client/src/hooks/useInfiniteGetImage.jsx
@@ -7,34 +7,17 @@ function useGetImageList(url) {
     const [loading, setLoading] = useState(true);
     const [fetchedData, setFetchedData] = useState([]);
 
-    // https://picsum.photos/ 더미 이미지 API
-    const getApiData = async () => {
-        try {
-            const config = {
-                params: {
-                    page: pageNum,
-                },
-            };
-            const response = await axios.get(url, config);
-            // console.log("response:", response.data);
-            setFetchedData([...response.data]);
-            setLoading(false);
-        } catch (error) {
-            console.error("데이터를 불러오는 중 오류 발생:", error);
-            // 오류 토스트 컴포넌트를 여기에 추가할 수 있습니다.
-            setLoading(false);
-        }
-    };
-
     useEffect(() => {
         const io = new IntersectionObserver((entries, observer) => {
             entries.forEach((entry) => {
+                console.log("옵저버 조건: 로딩완료: ", loading === false," 관측됨: ", entry.isIntersecting);
                 if (loading === false && entry.isIntersecting) {
                     setPageNum((prev) => prev + 1);
                 }
             });
+        },{
+            rootMargin: "60%",
         });
-
         if (sentinelRef.current) {
             io.observe(sentinelRef.current);
         }
@@ -44,10 +27,30 @@ function useGetImageList(url) {
             }
         };
     }, [loading]);
+
     useEffect(() => {
         getApiData();
     }, [pageNum]);
 
+    const getApiData = async () => {
+        try {
+            const config = {
+                params: {
+                    page: pageNum,
+                },
+            };
+
+            // https://picsum.photos/ 더미 이미지 API
+            const response = await axios.get(url, config);
+            console.log("response:", response.data);
+            setFetchedData([...response.data]);
+        } catch (error) {
+            console.error("데이터를 불러오는 중 오류 발생:", error);
+            // 오류 토스트 컴포넌트를 여기에 추가할 수 있습니다.
+        } finally {            
+            setTimeout(() => setLoading(false),3000);
+        }
+    };
     return { fetchedData, sentinelRef };
 }
 export default useGetImageList;
