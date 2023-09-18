1968d1a2e89b1ad45a6e3c68a51191fe3adf7c78
yejicho-helloworld <chos41379@gmail.com>
2023-09-18 17:06:34 +0900

    댓글컴포넌트수정 (#53)
    * 댓글컴포넌트수정
    
    * api주소 수정
    
    * 통신 연결 및 세부스타일링수정
    
    * 머지
    
    ---------
    
    Co-authored-by: 김형수 <85207564+gimhyoungsoo@users.noreply.github.com>

--- client/src/components/PostDetail/PostModal.jsx
+++ client/src/components/PostDetail/PostModal.jsx
@@ -3,6 +3,7 @@ import { createPortal } from "react-dom";
 import PostComponent from "./PostDetail";
 
 export default function Modal({ children }) {
+
     const [showPostModal, setShowPostModal] = useState(false);
 
     const renderModal = (bool) => {
