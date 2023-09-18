95653de968d8c7c6bd7d614828b77c6d9240dd34
Yeji Cho <chos41379@gmail.com>
2023-09-18 17:00:40 +0900

    머지

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -1,15 +1,1 @@
-import React from "react";
-import Modal from "../components/UploadModal/Modal";
-import PostComponent from "../components/PostData";
-import PostModal from "../components/PostDetail/PostModal";
 
-function App() {
-    return (
-        <div>            
-            {/* <Modal /> */}
-            <PostModal />
-        </div>
-    );
-}
-
-export default App;
